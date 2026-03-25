7b54c44eafcdddbd2d85d91bdf94acc9bf60a3c0
eliorocha <e.rocha1204@gmail.com>
2026-03-25 16:41:51 -0400

    feat: Dockerfile added

--- scripts/script.py
+++ scripts/script.py
@@ -2,9 +2,8 @@ import requests
 
 
 def main():
-    resposne = requests.get(url="http://localhost:8000/sleep/summary")
+    resposne = requests.get(url="http://0.0.0.0:8000/stress/summary/")
     print(resposne.json())
 
 if __name__ == "__main__":
     main()
-    
