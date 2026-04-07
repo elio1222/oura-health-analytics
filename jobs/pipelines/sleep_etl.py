155bc593ce346eba1ec6b18ed89abd236785479c
eliorocha <e.rocha1204@gmail.com>
2026-04-07 17:45:18 -0400

    refactor: sleep etl daily pipeline

--- jobs/pipelines/sleep_etl.py
+++ jobs/pipelines/sleep_etl.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, DateTime, Date, Integer, String
+from sqlalchemy import create_engine, DateTime, Date, Integer, String, func
 from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column, sessionmaker, Session
 from dotenv import load_dotenv
 from typing import List, Dict
@@ -6,15 +6,8 @@ from datetime import date, datetime
 import os
 from services.oura_service import fetch_oura_data 
 from dateutil.relativedelta import relativedelta
-import json
-import logging
 
 load_dotenv()
-
-logger = logging.getLogger(__name__)
-
-
-# sqlalchemy magic
 
 engine = create_engine(os.getenv("DATABASE_URI"))
 
@@ -39,93 +32,8 @@ class RawDailySleep(Base):
     timing: Mapped[int] = mapped_column(Integer) 
     total_sleep: Mapped[int] = mapped_column(Integer)
 
-# creates tables in sql from python objects 
-Base.metadata.create_all(engine)
-
-def param_builder(start_date: date, end_date: date) -> dict:
-    return {
-        "start_date": start_date,
-        "end_date": end_date
-    }
-
-# get raw sleep data from oura api and save raw data into postgresql
-"""Extract Raw Data From Oura API requests and save it into database"""
-def main():
-    def fetch_and_save_dailysleep():
-
-        # Session = sessionmaker(bind=engine)
-        # session = Session()
-
-        def extract_data(session: Session, data: dict):
-
-            logging.info("inside extract data function")
-
-            for d in data["data"]:
-                contributors = d.get("contributors", {})
-                dailysleep_obj = {
-                    "id": d.get("id"),
-                    "day": d.get("day"),
-                    "score": d.get("score"),
-                    "timestamp": d.get("timestamp"),
-                    "deep_sleep": contributors.get("deep_sleep"),
-                    "efficiency": contributors.get("efficiency"),
-                    "latency": contributors.get("latency"),
-                    "rem_sleep": contributors.get("rem_sleep"),
-                    "restfulness": contributors.get("restfulness"),
-                    "timing": contributors.get("timing"),
-                    "total_sleep": contributors.get("total_sleep")
-                }
-
-                new_dailysleep_record = RawDailySleep(**dailysleep_obj)
-                session.merge(new_dailysleep_record) # merge checks if the primary key already exists, if it does, it updates it, if not it adds it. better than session.add() which can cause duplicate primary key if ran more than once. 
-
-            pass
-
-        start_of_history = date(2024, 12, 1)
-        current_start = start_of_history
-        end_of_history = date.today()
-
-        while current_start < end_of_history:
-            current_end = current_start + relativedelta(months=1) - relativedelta(days=1)
-            
-            params = param_builder(start_date=current_start, end_date=current_end)
-
-            logging.info(f"Parameters testing: {params}")
-
-
-            try:
-                data = fetch_oura_data(url="https://api.ouraring.com/v2/usercollection/daily_sleep", params=params)
-
-                logging.info("data is fine")
-
-                Session = sessionmaker(bind=engine)
-                session = Session()
-
-                extract_data(session, data)
-
-                session.commit()
-
-                logging.info("extract_data was successful")
-
-
-
-            except Exception as e:
-                logging.info(e)
-                logging.info(f"{params} failed")
-
-            current_start += relativedelta(months=1)
-
-
-    fetch_and_save_dailysleep()
-
-
-
-
-
-# transform raw sleep data, remove certain fields, add fields, etc
-
 class DailySleep(Base):
-    __tablename__ = "sleep_data"
+    __tablename__ = "daily_sleep"
 
     # same fields from raw data
     id: Mapped[str] = mapped_column(String, primary_key=True)
@@ -147,13 +55,75 @@ class DailySleep(Base):
     consistency_score: Mapped[int] = mapped_column(Integer)
     custom_score: Mapped[int] = mapped_column(Integer)
 
-def fetch_and_transform():
+# creates tables in sql from python objects 
+Base.metadata.create_all(engine)
 
+def param_builder(start_date: date, end_date: date) -> dict:
+    return {
+        "start_date": start_date,
+        "end_date": end_date
+    }
+
+def fetch_and_extract():
+    """Extract Raw Data From Oura API requests and save it into database"""
+    def save_raw_data(session: Session, data: dict):
+
+        for d in data["data"]:
+            contributors = d.get("contributors", {})
+            dailysleep_obj = {
+                "id": d.get("id"),
+                "day": d.get("day"),
+                "score": d.get("score"),
+                "timestamp": d.get("timestamp"),
+                "deep_sleep": contributors.get("deep_sleep"),
+                "efficiency": contributors.get("efficiency"),
+                "latency": contributors.get("latency"),
+                "rem_sleep": contributors.get("rem_sleep"),
+                "restfulness": contributors.get("restfulness"),
+                "timing": contributors.get("timing"),
+                "total_sleep": contributors.get("total_sleep")
+            }
+
+            new_dailysleep_record = RawDailySleep(**dailysleep_obj)
+            session.merge(new_dailysleep_record) # merge checks if the primary key already exists, if it does, it updates it, if not it adds it. better than session.add() which can cause duplicate primary key if ran more than once. 
+
+        session.commit()
+
+    # start_of_history = date(2024, 12, 1)
+    # current_start = start_of_history
+    # end_of_history = date.today()
+    
     Session = sessionmaker(bind=engine)
 
-    start_of_history = date(2024, 12, 1)
-    current_start = start_of_history
-    end_of_history = date.today()
+    with Session() as session:
+
+        current_start = session.query(func.max(RawDailySleep.day)).scalar()
+        end_of_history = date.today()
+
+        while current_start < end_of_history:
+            current_end = current_start + relativedelta(months=1) - relativedelta(days=1)
+            params = param_builder(start_date=current_start, end_date=current_end)
+            print(params)
+            try:
+                data = fetch_oura_data(url="https://api.ouraring.com/v2/usercollection/daily_sleep", params=params)
+                save_raw_data(session, data)
+            except Exception as e:
+                print(e)
+
+            current_start += relativedelta(months=1)
+
+# transform raw sleep data, remove certain fields, add fields, etc and load into postgresql database
+def transform_and_load():
+    """Transforms Raw Data from RawDailySleep table and loads it into a new table"""
+    Session = sessionmaker(bind=engine)
+
+    # start_of_history = date(2024, 12, 1)
+    # current_start = start_of_history
+    # end_of_history = date.today()
+
+    with Session() as session:
+        current_start = session.query(func.max(RawDailySleep.day)).scalar()
+        end_of_history = date.today()
 
     while current_start < end_of_history:
         current_end = current_start + relativedelta(months=1) - relativedelta(days=1)
@@ -161,11 +131,10 @@ def fetch_and_transform():
         with Session() as session:
 
             try:
-                data = session.query(RawDailySleep).filter(RawDailySleep.day >= current_start, RawDailySleep.day < current_end).all()
+                data = session.query(RawDailySleep).filter(RawDailySleep.day >= current_start, RawDailySleep.day <= current_end).all()
 
                 for d in data:
                     # formulas
-
                     performance_score = (0.25 * d.rem_sleep) + (0.20 * d.efficiency) + (0.20 * d.latency) + (0.15 * d.total_sleep) + (0.10 * d.deep_sleep) + (0.10 * d.timing)
 
                     recovery_score = (0.30 * d.deep_sleep) + (0.25 * d.total_sleep) + (0.15 * d.restfulness) + (0.10 * d.efficiency) + (0.10 * d.rem_sleep) + (0.10 * d.timing)
@@ -174,6 +143,7 @@ def fetch_and_transform():
 
                     custom_score = (0.25 * d.total_sleep) + (0.20 * d.rem_sleep) + (0.20 * d.efficiency) + (0.15 * d.latency) + (0.1 * d.deep_sleep) + (0.10 * d.timing)
 
+                    # transformed data obj
                     sleep_data = {
 
                         # fields from raw data
@@ -197,18 +167,24 @@ def fetch_and_transform():
 
                     transformed_data = DailySleep(**sleep_data)
                     session.merge(transformed_data)
-                    session.commit()
+                session.commit()
 
             except Exception as e:
-                logging.info(e)
-                logging.info(param_builder(start_date=current_start, end_date=current_end) + " failed")
+                print(e)
 
         current_start += relativedelta(months=1)
 
-# load to postgresql database
+
+# etl pipeline (run dailly pipeline)
+def run_daily_pipeline():
+    try:
+        fetch_and_extract()
+    except Exception as e:
+        return
+    try:
+        transform_and_load()
+    except Exception as e:
+        return
 
 if __name__ == "__main__":
-    logging.basicConfig(filename="sleep_etl.log", level=logging.INFO)
-    logging.info("Starting main")
-    fetch_and_transform()
-    logging.info("success")
+    run_daily_pipeline()
